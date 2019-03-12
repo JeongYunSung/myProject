7d202a9642a5bb5ba3fbf93caab6629ebef9769e
YunSeong-Jeong <123dbstjd@naver.com>
2019-03-13 04:51:24 +0900

    build.gradle수정

--- src/main/java/com/teddy_pear/four/TestController.java
+++ src/main/java/com/teddy_pear/four/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-    @RequestMapping("/good")
+    @RequestMapping("/test")
     public String get(){
         return "test";
     }
