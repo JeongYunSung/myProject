b6b52338899dab5f8d3d3f538190fca6fd01ba88
YunSeong-Jeong <123dbstjd@naver.com>
2019-03-13 03:50:57 +0900

    test

--- src/main/java/com/teddy_pear/four/TestController.java
+++ src/main/java/com/teddy_pear/four/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
+    @RequestMapping("/good")
     public String get(){
         return "test";
     }
