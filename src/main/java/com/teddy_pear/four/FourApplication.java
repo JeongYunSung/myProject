a3c9e810094efbec1614ec7107df7f50d2897be2
YunSeong-Jeong <123dbstjd@naver.com>
2019-03-14 02:42:19 +0900

    java,war

--- src/main/java/com/teddy_pear/four/FourApplication.java
+++ src/main/java/com/teddy_pear/four/FourApplication.java
@@ -2,9 +2,10 @@ package com.teddy_pear.four;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FourApplication {
+public class FourApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FourApplication.class, args);
